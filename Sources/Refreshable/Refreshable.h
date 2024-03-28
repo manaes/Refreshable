8c471e11f879f79c37e00af29530e84256c3f3a7
wanny.park <wanny.park@pandora.tv>
2024-03-28 15:56:31 +0900

    carthage 지원 추가

---
+++ Sources/Refreshable/Refreshable.h
@@ -0,0 +1,18 @@
+//
+//  Refreshable.h
+//  Refreshable
+//
+//  Created by manaes on 3/28/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Refreshable.
+FOUNDATION_EXPORT double RefreshableVersionNumber;
+
+//! Project version string for Refreshable.
+FOUNDATION_EXPORT const unsigned char RefreshableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Refreshable/PublicHeader.h>
+
+
